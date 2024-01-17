f0d753ad4b58828943c469d77648d3cafa617729
Wyllyam Vieira <wyllyambr80@gmail.com>
2024-01-17 08:01:13 -0400

    criando backup do banco de dados

---
+++ bancodedados.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.4.28-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              12.5.0.6677
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Copiando estrutura para tabela sistema.clientes
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `estadoCivil` varchar(50) DEFAULT NULL,
+  `conjuge` varchar(255) DEFAULT NULL,
+  `mensagem` varchar(255) NOT NULL,
+  `profissao` varchar(100) NOT NULL,
+  `identidade` varchar(20) NOT NULL,
+  `orgaoemissor` varchar(100) NOT NULL,
+  `CPF` varchar(14) NOT NULL,
+  `namep` varchar(255) NOT NULL,
+  `namem` varchar(255) NOT NULL,
+  `datanascimento` date NOT NULL,
+  `cidadadenatal` varchar(100) NOT NULL,
+  `endereco` varchar(255) NOT NULL,
+  `telefone` varchar(20) NOT NULL,
+  `sc` varchar(255) NOT NULL,
+  `pfinal` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fpagamento` varchar(50) NOT NULL,
+  `vservico` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela sistema.clientes: ~0 rows (aproximadamente)
+
+-- Copiando estrutura para tabela sistema.usuarios
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `username` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Copiando dados para a tabela sistema.usuarios: ~0 rows (aproximadamente)
+INSERT INTO `usuarios` (`username`, `password`) VALUES
+	('wyllyam', 'wyllyam');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
